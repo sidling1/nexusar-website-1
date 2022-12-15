cbdc2beea8b9a495f0ffec6d967565acc2ee0983
sidjethwani@gmail.com <sidjethwani@gmail.com>
2022-12-15 19:09:00 +0530

    Card 2 changed

--- src/components/Solutions/CardTwo/CardTwo.js
+++ src/components/Solutions/CardTwo/CardTwo.js
@@ -1,23 +1,36 @@
 import { Box } from '@mui/system';
-import { Container, Grid, Paper } from '@mui/material';
+import { Button, Container, Grid, Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
 import classes from './CardTwo.module.css';
-import './CardTwo.module.css'
 
 const CardTwo = (props) => {
   const { heading, highlights, content,image } = props;
 
   return (
-    <div className="mainblock">
-      <div className="image">
-        <img alt="card-2" src={image} />
-      </div>
-      <div className="content">
-        <h3 className="bigHeader" style={{color:"white"}}>{heading}</h3>
-        <p style={{fontSize: "21px", color: "white"}}>{highlights}{content}</p>
-        <button>{heading}</button>
-      </div>
-    </div>
+    <Paper elevation={4} className={classes.card}>
+      <Grid container item xs={12} sm={6} spacing={0} direction="column">
+        <Grid item xs={6} md={6}>
+          <Container sx={{ mb: 1, mr:2, overflow: 'hidden'}}>
+            <img alt="card-2" className={classes.image} src={image} />
+          </Container>
+        </Grid>
+        <Grid container item xs={12} md={12} direction="column" alignItems="center">
+            <Container sx={{ py: 4}}>
+              <h3 style={{textAlign:"center"}} className={classes.bigHeader}>{heading}</h3>
+            </Container>
+          
+            <Container fixed sx={{height: "43%",overflow:"auto"}}>
+              <p style={{fontSize: "21px"}}>
+                {highlights}{content}
+              </p>
+              <Box sx={{ py: 2 }} />
+            </Container>
+        
+            <Button variant="contained" size="medium" sx={{my:1,fontSize:15,alignSelf:"center",color:"#134e5e",backgroundColor:"white",':hover':{bgcolor:"grey"}}}>More Info</Button>
+          
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
